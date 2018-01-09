9ea6f73a08c7b3459793eb203141f41686450d50
Scott Turnbull <streamweaver@gmail.com>
2018-01-09 17:09:51 -0500

    Fees and rents calculated on manor class.

--- src/app/manors/shared/models/manor.model.ts
+++ src/app/manors/shared/models/manor.model.ts
@@ -1,4 +1,7 @@
 import {IPopulation, Population} from './population.model';
+import {TenantClass} from '../generators/tenant-generator';
+import {Officer} from '../generators/tenant-officer.generator';
+import {craftsmanFees, Craftsman} from '../generators/craftsman-generator';
 
 export class IManor {
   name: string;
@@ -38,6 +41,7 @@ class Manor implements IManor {
   landQuality: number;
   population: Population;
   freeRent: number;
+  baseRent: number;
   serfLabor: number;
   isSlaveState: boolean;
   foAcresPerHH: number;
@@ -53,10 +57,44 @@ class Manor implements IManor {
     this.landQuality = 1.0;
     this.population = new Population();
     this.freeRent = 6;
+    this.baseRent = 60;
     this.serfLabor = 4;
     this.isSlaveState = false;
     this.foAcresPerHH = 1500;
     this.foAcresPerLF = 600;
     this.Notes = [];
+  }
+
+  setTenantFees() {
+      for (const t of this.population.tenants) { // TODO check these base amounts.
+        t.fees = (t.occupation === TenantClass.SLAVE) ? 0 : 6 + t.free_acres + t.serf_acres; // from tenant gen
+        if (t.craft !== null) {
+          t.fees += craftsmanFees[t.craft];
+        }
+        if (t.craft === Craftsman.GMDetermine) { // from craftsman generator
+          t.notes.push('Guild fees not set yet'); // TODO find way to make note idempotent
+        } else {
+          t.notes.push('Guild fees of ' + craftsmanFees[t.craft] + 'd included');
+        }
+        if (t.military !== null) {
+          t.fees = 60 + t.free_acres;
+        }
+        if(t.office === Officer.Glebe) {
+          t.fees = 0;
+        }
+      }
+  }
+
+  setTenantRent() {
+    for (const tenant of this.population.tenants) {
+      const base = (tenant.occupation === TenantClass.SLAVE) ? 0 : this.baseRent;
+      tenant.rent = base + this.freeRent * tenant.free_acres;
+      if (tenant.military !== null) {  // Adjust for military.
+        tenant.rent = 60 + tenant.free_acres;
+      }
+      if (tenant.office === Officer.Glebe) { // Glebe's pay no rent.
+        tenant.rent = 0;
+      }
+    }
   }
 }
