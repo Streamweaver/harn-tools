8d0bf69aa172c1ee19899bee99c5f9215a41dbb9
Scott Turnbull <streamweaver@gmail.com>
2018-02-09 23:12:21 -0500

    Prices working now

--- src/app/prices/price-list/price-list.component.ts
+++ src/app/prices/price-list/price-list.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import * as _ from 'lodash';
-import {Observable} from 'rxjs/Observable';
 import {PriceListing} from '../shared/price-listing.model';
 import {PriceService} from '../shared/price.service';
 import 'rxjs/add/operator/debounceTime';
@@ -19,35 +18,38 @@ export class PriceListComponent implements OnInit {
   vendors: string[];
   searchField: FormControl;
 
-  // filter-able properties
-  name: string;
-  vendor: string;
+  filters: {[key: string]: any};
+  currentVendor: string;
+  currentSearchTerm: string;
 
-  filters = {};
-
-  constructor(
-    private priceService: PriceService
-  ) {
+  constructor(private priceService: PriceService) {
   }
 
   ngOnInit() {
+    this.clearFilters();
     this.vendors = [];
-    this.pricesReady = true;
+    this.pricesReady = false;
     this.searchField = new FormControl();
+    this.filteredPrices = [];
     this.searchField.valueChanges
       .debounceTime(400)
       .distinctUntilChanged()
       .subscribe(term => {
         this.onSearchPrices(term);
       });
-    this.priceService.getPriceList().subscribe(
-      priceList => this.filteredPrices = priceList,
-      err => console.log(err),
-      () => this.pricesReady = true
+
+    this.priceService.priceList.subscribe(
+      priceList => this.priceList = priceList,
+          err => console.log(err),
+          () => {
+            this.pricesReady = true;
+            this.parseVendors();
+          }
     );
+    this.parseVendors();
   }
 
-  localPrices(price: number): { coin: string, amount: number}[] {
+  localPrices(price: number): {coin: string, amount: number}[] {
     const priceLabel: { coin: string, amount: number}[] = [];
     if (price / 240 > 1) {
       priceLabel.push({coin: 'L', amount: Math.floor(price / 240)});
@@ -64,19 +66,64 @@ export class PriceListComponent implements OnInit {
   }
 
   onSearchPrices(term: string) {
-    this.priceService.filterIncludes('name', term);
+    this.currentSearchTerm = term;
+    this.filterIncludes('name', term);
   }
 
   onVendorSelect(rule: string) {
-    this.priceService.filterExact('vendor', rule);
+    this.currentVendor = rule;
+    this.filterExact('vendor', rule);
   }
 
-  private parseVendorList() {
-    this.vendors = [];
+  private parseVendors() {
     for (const item of this.priceList) {
       if (this.vendors.indexOf(item.vendor) < 0) {
         this.vendors.push(item.vendor);
       }
-    this.vendors = this.vendors.sort();
+      this.vendors = this.vendors.sort();
+    }
   }
-}}
+
+  // Filtering prices
+  private applyFilters() {
+    if (this.isFilterEmpty()) {
+      this.filteredPrices = [];
+    } else {
+      this.filteredPrices = _.filter(this.priceList, _.conforms(this.filters));
+    }
+    }
+
+  private filterExact(property: string, rule: string) {
+      if (this.filters[property] !== rule) {
+        if (!rule) {
+          this.removeFilter(property);
+        } else {
+          this.filters[property] = val => val === rule;
+        }
+        this.applyFilters();
+      }
+    }
+
+    filterIncludes(property: string, rule: string) {
+      if (this.filters[property] !== rule) {
+        if (!rule) {
+          this.removeFilter(property);
+        } else {
+          this.filters[property] = val => val.includes(rule.toLowerCase());
+        }
+        this.applyFilters();
+      }
+    }
+
+    removeFilter(property: string) {
+      delete this.filters[property];
+    }
+
+    clearFilters() {
+      this.filters = {};
+    }
+
+    isFilterEmpty() {
+    return _.isEmpty(this.filters);
+    }
+}
--- src/app/prices/shared/price.service.ts
+++ src/app/prices/shared/price.service.ts
@@ -1,84 +1,19 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Observable} from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
 import { PriceListing } from './price-listing.model';
 import * as _ from 'lodash';
 
 @Injectable()
 export class PriceService {
-  private fullPriceList: PriceListing[];
-  private _priceSubject: Subject<PriceListing[]>;
-
-  filters = {};
+  priceList: Observable<PriceListing[]>;
 
   constructor(private http: HttpClient) {
-    this.http.get<PriceListing[]>('assets/data/combinedprices.data.json').subscribe(
-      priceList => this.fullPriceList = priceList,
-    );
+    this.loadPrices();
   }
 
-  getPriceList(): Observable<PriceListing[]> {
-    return this._priceSubject.asObservable();
+  loadPrices() {
+    this.priceList = this.http.get<PriceListing[]>('assets/data/price.data.json');
   }
-
-  private applyFilters() {
-    this._priceSubject.next(_.filter(this.fullPriceList, _.conforms(this.filters)));
-  }
-
-  // Much of this pulled from the very find AngularFirebase tutorial at
-  // https://angularfirebase.com/lessons/multi-property-data-filtering-with-firebase-and-angular-4/
-  /// filter property by equality to rule
-  filterExact(property: string, rule: any) {
-    if (this.filters[property] !== rule) {
-      if (!rule) {
-        this.removeFilter(property);
-      } else {
-        this.filters[property] = val => val === rule;
-        this.applyFilters();
-      }
-    }
-  }
-
-  filterIncludes(property: string, rule: string) {
-    if (this.filters[property] !== rule) {
-      if (!rule) {
-        this.removeFilter(property);
-      } else {
-        this.filters[property] = val => val.includes(rule.toLowerCase());
-        this.applyFilters();
-      }
-    }
-  }
-
-  /// filter  numbers greater than rule
-  filterLessThan(property: string, rule: number) {
-    if (this.filters[property] !== rule) {
-      if (!rule) {
-        this.removeFilter(property);
-      } else {
-        this.filters[property] = val => val < rule;
-        this.applyFilters();
-      }
-    }
-  }
-
-  /// filter properties that resolve to true
-  filterBoolean(property: string, rule: boolean) {
-    if (this.filters[property] !== rule) {
-      if (!rule) {
-        this.removeFilter(property);
-      } else {
-        this.filters[property] = val => val;
-        this.applyFilters();
-      }
-    }
-  }
-
-  /// removes filter
-  removeFilter(property: string) {
-    delete this.filters[property];
-    this.applyFilters();
-  }
-
 }
